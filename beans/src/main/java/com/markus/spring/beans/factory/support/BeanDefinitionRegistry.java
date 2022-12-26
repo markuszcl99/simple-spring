5bf2f1ee37994183e3bf7eff3b72745fda9e74ee
markuszcl99 <e_markuszhang@163.com>
2022-12-26 22:25:43 +0800

    定义BeanDefinitionRegistry注册器

---
+++ beans/src/main/java/com/markus/spring/beans/factory/support/BeanDefinitionRegistry.java
@@ -0,0 +1,15 @@
+package com.markus.spring.beans.factory.support;
+
+import com.markus.spring.beans.factory.config.BeanDefinition;
+
+/**
+ * @author: markus
+ * @date: 2022/12/26 10:18 PM
+ * @Description: BeanDefinition 注册表
+ * @Blog: http://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public interface BeanDefinitionRegistry {
+
+    void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
+}
