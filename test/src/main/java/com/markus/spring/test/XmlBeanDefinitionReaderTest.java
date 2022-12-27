04e46480a73cc807c2c2c9501a5a9964d5caf94b
markuszcl99 <e_markuszhang@163.com>
2022-12-27 23:19:56 +0800

    资源加载测试类编写完成并测试通过

---
+++ test/src/main/java/com/markus/spring/test/XmlBeanDefinitionReaderTest.java
@@ -0,0 +1,20 @@
+package com.markus.spring.test;
+
+import com.markus.spring.beans.factory.support.BeanDefinitionRegistry;
+import com.markus.spring.beans.factory.support.DefaultListableBeanFactory;
+import com.markus.spring.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * @author: markus
+ * @date: 2022/12/27 11:03 PM
+ * @Description:
+ * @Blog: http://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public class XmlBeanDefinitionReaderTest {
+    public static void main(String[] args) {
+        BeanDefinitionRegistry registry = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(registry);
+        reader.loadBeanDefinitions("classpath:/META-INF/application-context.xml");
+    }
+}
