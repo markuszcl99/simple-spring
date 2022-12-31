b9f78f319f9a8e2d33542d4c61e67bd9ce1c1847
markuszcl99 <e_markuszhang@163.com>
2022-12-31 22:44:05 +0800

    创建实例测试代码编写且测试通过

---
+++ test/src/main/java/com/markus/spring/test/BeanFactoryDependencyLookupTest.java
@@ -0,0 +1,24 @@
+package com.markus.spring.test;
+
+import com.markus.spring.beans.factory.BeanFactory;
+import com.markus.spring.beans.factory.support.DefaultListableBeanFactory;
+import com.markus.spring.beans.factory.xml.XmlBeanDefinitionReader;
+import com.markus.spring.domain.User;
+
+/**
+ * @author: markus
+ * @date: 2022/12/31 10:35 PM
+ * @Description: IoC容器依赖查找测试
+ * @Blog: http://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public class BeanFactoryDependencyLookupTest {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:/META-INF/application-context.xml");
+
+        User user = (User) beanFactory.getBean("user");
+        System.out.println(user);
+    }
+}
