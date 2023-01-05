23acef47329736ec7cd0855c92a31218b0e37c85
markuszcl99 <e_markuszhang@163.com>
2023-01-05 22:40:53 +0800

    Aware接口回调功能测试代码编写以及测试通过

--- test/src/main/java/com/markus/spring/domain/User.java
+++ test/src/main/java/com/markus/spring/domain/User.java
@@ -1,5 +1,10 @@
 package com.markus.spring.domain;
 
+import com.markus.spring.beans.factory.BeanClassLoaderAware;
+import com.markus.spring.beans.factory.BeanFactory;
+import com.markus.spring.beans.factory.BeanFactoryAware;
+import com.markus.spring.beans.factory.BeanNameAware;
+
 /**
  * @author: markus
  * @date: 2022/12/29 11:45 PM
@@ -7,9 +12,12 @@ package com.markus.spring.domain;
  * @Blog: http://markuszhang.com
  * It's my honor to share what I've learned with you!
  */
-public class User {
+public class User implements BeanNameAware, BeanFactoryAware, BeanClassLoaderAware {
     private String username;
     private String sex;
+    private ClassLoader classLoader;
+    private String beanName;
+    private BeanFactory beanFactory;
 
     public User() {
     }
@@ -35,11 +43,45 @@ public class User {
         this.sex = sex;
     }
 
+    public ClassLoader getClassLoader() {
+        return classLoader;
+    }
+
+    public void setClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
                 ", sex='" + sex + '\'' +
+                ", classLoader=" + classLoader +
+                ", beanName='" + beanName + '\'' +
+                ", beanFactory=" + beanFactory +
                 '}';
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
     }
 }
