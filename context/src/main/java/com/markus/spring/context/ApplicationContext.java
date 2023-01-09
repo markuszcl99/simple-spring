91d75a46bec67a51e8cca75b43edc46352af50e3
markuszcl99 <e_markuszhang@163.com>
2023-01-09 22:49:29 +0800

    随便提交一下

---
+++ context/src/main/java/com/markus/spring/context/ApplicationContext.java
@@ -0,0 +1,11 @@
+package com.markus.spring.context;
+
+/**
+ * @author: markus
+ * @date: 2023/1/9 10:48 PM
+ * @Description: 应用上下文
+ * @Blog: http://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public interface ApplicationContext {
+}
